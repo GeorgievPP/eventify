f4f3d878a5e2c8e924e6bfaf0ac32e052845a4bc
Petyo Georgiev <calevra1807@abv.bg>
2026-02-01 23:31:58 +0200

    chore: update API base URL for production

--- src/app/core/constants/api.config.ts
+++ src/app/core/constants/api.config.ts
@@ -4,5 +4,5 @@ import { environments } from "../../environments/environments";
 
 
 // export const API_BASE_URL = environments.apiUrl;
-export const API_BASE_URL = 'http://localhost:5000/api';
-// export const API_BASE_URL = environments.apiUrl;
+// export const API_BASE_URL = 'http://localhost:5000/api';
+export const API_BASE_URL = environments.apiUrl;
